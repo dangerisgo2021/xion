e01da6337f19ce859af65a108dcd5f785709a284
Clark <thecluskin@gmail.com>
2020-06-05 23:13:06 -0400

    added default values for typescript inference

--- src/app/auth/useAuth.ts
+++ src/app/auth/useAuth.ts
@@ -1,5 +1,17 @@
 import React, { useContext } from "react";
 
-export const AuthContext = React.createContext({user: null});
+export const AuthContext = React.createContext({
+  isAuthenticated: false,
+  user: null,
+  loading: false,
+  popupOpen: false,
+  loginWithPopup: undefined,
+  handleRedirectCallback: undefined,
+  getIdTokenClaims: undefined,
+  loginWithRedirect: undefined,
+  getTokenSilently: undefined,
+  getTokenWithPopup: undefined,
+  logout: undefined,
+});
 
 export const useAuth = () => useContext(AuthContext);
