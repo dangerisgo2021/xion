fe08bf058ba39edb1d35f295b8e0e42ed55584e5
Clark <thecluskin@gmail.com>
2020-06-15 02:14:06 -0400

    added production settings
    removed console.log

--- src/app/auth/auth0Config.ts
+++ src/app/auth/auth0Config.ts
@@ -3,3 +3,4 @@ export const auth0Config = {
   client_id: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID,
   redirect_uri: process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI,
 };
+console.log("process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI : ", process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI)
