9bffb4656b0a6b6a3c31579b4635ffc64986d8ef
Clark <thecluskin@gmail.com>
2020-06-27 00:34:47 -0400

    need to not call __REDUX_DEVTOOLS_EXTENSION__ when it is not a function

--- src/app/state/redux.config.ts
+++ src/app/state/redux.config.ts
@@ -6,8 +6,12 @@ const getReduxDevToolsEnhancer = () => {
     return [];
   } else {
     //cannot reference window on server
-    // @ts-ignore __REDUX_DEVTOOLS_EXTENSION__ is injected by the extension
-    const reduxDevToolsEnhancer = window?.__REDUX_DEVTOOLS_EXTENSION__();
+
+    const reduxDevToolsEnhancer =
+      // @ts-ignore __REDUX_DEVTOOLS_EXTENSION__ is injected by the extension
+      typeof window?.__REDUX_DEVTOOLS_EXTENSION__ === "function" &&
+      // @ts-ignore __REDUX_DEVTOOLS_EXTENSION__ is injected by the extension
+      window?.__REDUX_DEVTOOLS_EXTENSION__();
 
     if (!reduxDevToolsEnhancer) {
       return [];
