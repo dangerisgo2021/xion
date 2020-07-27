05c51fd1b6edb45c93ea0ffc90216286b14ec176
Clark <thecluskin@gmail.com>
2020-07-27 01:04:48 -0400

    removed rooms from nav

--- src/app/nav/reducers/navReducer.ts
+++ src/app/nav/reducers/navReducer.ts
@@ -4,7 +4,6 @@ export const navReducer = new ReducerBuilder()
   .setInitialState({
     items: {
       home: { href: "/", displayText: "Active Orders", id: "home" },
-      checkout: { href: "/rooms", displayText: "Rooms", id: "rooms" },
       catalog: { href: "/catalog", displayText: "Catalog", id: "catalog" },
       forms: { href: "/forms", displayText: "Forms", id: "forms" },
       reports: { href: "/reports", displayText: "Reports", id: "reports" },
